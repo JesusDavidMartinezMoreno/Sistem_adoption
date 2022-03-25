6e5353524fe742f5e549a1a924bdd7d6f1e54d4d
JesusDavidMartinezMoreno <martinezjesus1cm@gmail.com>
2022-03-24 22:14:26 -0600

    primer commit

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bd7229d334d7fd",
+	password: "cced70f2",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_5f028e3f937c96d",
 	secret_key:"k6oj9d9xZYQSwV"
 };
 exports.config = config;
